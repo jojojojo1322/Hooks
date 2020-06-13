28a9194112825702a6bfea7190355a4ecee90eb9
jojojojo1322 <bobob935@gmail.com>
2020-06-13 17:44:36 +0900

    useConfirm

--- src/App.js
+++ src/App.js
@@ -1,89 +1,24 @@
-// import React, { Component, useState, useEffect, useRef } from "react";
-// import ReactDom from "react-dom";
-// import "./App.css";
-
-// const useClick = (onClick) => {
-//   const element = useRef();
-//   useEffect(() => {
-//     if (element.current) {
-//       element.current.addEventListener("click", onClick);
-//     }
-//     return () => {
-//       if (element.current) {
-//         element.current.removeEventListener("click", onClick);
-//       }
-//     };
-//   }, []);
-//   return element;
-// };
-
-// const App = () => {
-//   const sayHello = () => console.log("say Hello!");
-//   const title = useClick(sayHello);
-
-//   return (
-//     <div className="App">
-//       <h1 ref={title}>HOOKS</h1>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-// import React, { useState, useRef, useEffect } from "react";
-// import "./App.css";
-
-// const useClick = (onClick) => {
-//   const element = useRef();
-//   useEffect(() => {
-//     if (element.current) {
-//       element.current.addEventListener("click", onClick);
-//     }
-//     return (
-//       () => {
-//         if (element.current) {
-//           element.current.removeEventListener("click", onClick);
-//         }
-//       },
-//       []
-//     );
-//   });
-//   return element;
-// };
-
-// const App = () => {
-//   const sayHello = () => console.log("sayhello");
-//   const potato = useClick(sayHello);
-
-//   return (
-//     <div className="App">
-//       <h1 ref={potato}>Hooks</h1>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-import React, { useRef, useEffect } from "react";
+import React, { Component, useEffect, useState, useRef } from "react";
 import "./App.css";
 
-const useClick = (onClick) => {
-  const element = useRef();
-  useEffect(() => {
-    if (element.current) {
-      element.current.addEventListener("click", onClick);
+const useConfirm = (message = "", callback) => {
+  if (typeof callback !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (window.confirm(message)) {
+      callback();
     }
-  });
-  return element;
+  };
+  return confirmAction;
 };
 
 const App = () => {
-  const sayHello = () => console.log("say");
-  const title = useClick(sayHello);
-
+  const deleteWorld = () => console.log("delete world!");
+  const confirmDelete = useConfirm("REAL?", deleteWorld);
   return (
     <div className="App">
-      <h1 ref={title}>HOOKS</h1>
+      <button onClick={confirmDelete}>Delete the world</button>
     </div>
   );
 };
